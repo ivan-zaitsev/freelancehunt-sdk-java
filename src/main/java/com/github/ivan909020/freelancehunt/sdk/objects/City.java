f98e00ec2958cd642c2e9eb149e279df124ab9e1
Ivan Zaitsev <ivan909020@gmail.com>
2020-06-01 11:07:19 +0300

    Created request and response for cities

---
+++ src/main/java/com/github/ivan909020/freelancehunt/sdk/objects/City.java
@@ -0,0 +1,48 @@
+package com.github.ivan909020.freelancehunt.sdk.objects;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Objects;
+
+public class City {
+
+    @JsonProperty("id")
+    private Long id;
+
+    @JsonProperty("name")
+    private String name;
+
+    public City() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        City country = (City) o;
+        return Objects.equals(id, country.id) &&
+                Objects.equals(name, country.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+}
---
+++ src/main/java/com/github/ivan909020/freelancehunt/sdk/responses/get/GetCitiesResponse.java
@@ -0,0 +1,21 @@
+package com.github.ivan909020.freelancehunt.sdk.responses.get;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.github.ivan909020.freelancehunt.sdk.objects.City;
+import com.github.ivan909020.freelancehunt.sdk.responses.ApiResponse;
+
+import java.util.List;
+
+public class GetCitiesResponse extends ApiResponse {
+
+    @JsonProperty("data")
+    private List<City> cities;
+
+    public GetCitiesResponse() {
+    }
+
+    public List<City> getCities() {
+        return cities;
+    }
+
+}
